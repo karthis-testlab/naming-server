9d1dba0e84d6010de2018de7df9568776821de14
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2023-04-14 11:47:11 +0530

    Adding naming server in remote repo'

---
+++ src/main/java/com/ta/api/assessment/naming/server/namingserver/NamingServerApplication.java
@@ -0,0 +1,15 @@
+package com.ta.api.assessment.naming.server.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+
+}
